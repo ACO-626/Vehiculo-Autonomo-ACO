9af8e95ef5326b31cbe466628cd571518c4adf0f
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2021-01-17 07:54:33 -0600

    Agregar archivos de proyecto.

---
+++ FormPrincipal.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Vehículo_Autónomo_ACO
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.pictureLogo = new System.Windows.Forms.PictureBox();
+            this.lbTittle = new System.Windows.Forms.Label();
+            this.lbSimpleSubtittle = new System.Windows.Forms.Label();
+            this.pictureVideo = new System.Windows.Forms.PictureBox();
+            this.picturePerspective = new System.Windows.Forms.PictureBox();
+            this.picture1 = new System.Windows.Forms.PictureBox();
+            this.picture2 = new System.Windows.Forms.PictureBox();
+            this.lbCredits = new System.Windows.Forms.Label();
+            this.btnPlayer = new System.Windows.Forms.PictureBox();
+            this.btnImport = new System.Windows.Forms.PictureBox();
+            this.btnReiniciar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVideo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePerspective)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnImport)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnReiniciar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureLogo
+            // 
+            this.pictureLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureLogo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureLogo.Image = ((System.Drawing.Image)(resources.GetObject("pictureLogo.Image")));
+            this.pictureLogo.Location = new System.Drawing.Point(12, 12);
+            this.pictureLogo.Name = "pictureLogo";
+            this.pictureLogo.Size = new System.Drawing.Size(102, 89);
+            this.pictureLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureLogo.TabIndex = 0;
+            this.pictureLogo.TabStop = false;
+            // 
+            // lbTittle
+            // 
+            this.lbTittle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbTittle.AutoSize = true;
+            this.lbTittle.Font = new System.Drawing.Font("Montserrat", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTittle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbTittle.Location = new System.Drawing.Point(1163, 31);
+            this.lbTittle.Name = "lbTittle";
+            this.lbTittle.Size = new System.Drawing.Size(210, 24);
+            this.lbTittle.TabIndex = 1;
+            this.lbTittle.Text = "Vehículo Autónomo";
+            // 
+            // lbSimpleSubtittle
+            // 
+            this.lbSimpleSubtittle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbSimpleSubtittle.AutoSize = true;
+            this.lbSimpleSubtittle.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSimpleSubtittle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbSimpleSubtittle.Location = new System.Drawing.Point(1207, 55);
+            this.lbSimpleSubtittle.Name = "lbSimpleSubtittle";
+            this.lbSimpleSubtittle.Size = new System.Drawing.Size(166, 19);
+            this.lbSimpleSubtittle.TabIndex = 2;
+            this.lbSimpleSubtittle.Text = "Video segmentador";
+            // 
+            // pictureVideo
+            // 
+            this.pictureVideo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureVideo.Location = new System.Drawing.Point(146, 156);
+            this.pictureVideo.Name = "pictureVideo";
+            this.pictureVideo.Size = new System.Drawing.Size(896, 561);
+            this.pictureVideo.TabIndex = 6;
+            this.pictureVideo.TabStop = false;
+            // 
+            // picturePerspective
+            // 
+            this.picturePerspective.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.picturePerspective.Location = new System.Drawing.Point(1048, 156);
+            this.picturePerspective.Name = "picturePerspective";
+            this.picturePerspective.Size = new System.Drawing.Size(296, 178);
+            this.picturePerspective.TabIndex = 7;
+            this.picturePerspective.TabStop = false;
+            // 
+            // picture1
+            // 
+            this.picture1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.picture1.Location = new System.Drawing.Point(1048, 347);
+            this.picture1.Name = "picture1";
+            this.picture1.Size = new System.Drawing.Size(296, 178);
+            this.picture1.TabIndex = 8;
+            this.picture1.TabStop = false;
+            // 
+            // picture2
+            // 
+            this.picture2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.picture2.Location = new System.Drawing.Point(1048, 539);
+            this.picture2.Name = "picture2";
+            this.picture2.Size = new System.Drawing.Size(296, 178);
+            this.picture2.TabIndex = 9;
+            this.picture2.TabStop = false;
+            // 
+            // lbCredits
+            // 
+            this.lbCredits.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbCredits.AutoSize = true;
+            this.lbCredits.Font = new System.Drawing.Font("Montserrat", 7F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCredits.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbCredits.Location = new System.Drawing.Point(1294, 744);
+            this.lbCredits.Name = "lbCredits";
+            this.lbCredits.Size = new System.Drawing.Size(90, 13);
+            this.lbCredits.TabIndex = 10;
+            this.lbCredits.Text = "Creado por ACO";
+            // 
+            // btnPlayer
+            // 
+            this.btnPlayer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlayer.Image = ((System.Drawing.Image)(resources.GetObject("btnPlayer.Image")));
+            this.btnPlayer.Location = new System.Drawing.Point(46, 274);
+            this.btnPlayer.Name = "btnPlayer";
+            this.btnPlayer.Size = new System.Drawing.Size(68, 51);
+            this.btnPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnPlayer.TabIndex = 11;
+            this.btnPlayer.TabStop = false;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnImport.Image = ((System.Drawing.Image)(resources.GetObject("btnImport.Image")));
+            this.btnImport.Location = new System.Drawing.Point(46, 156);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(68, 57);
+            this.btnImport.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnImport.TabIndex = 12;
+            this.btnImport.TabStop = false;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReiniciar.Image = ((System.Drawing.Image)(resources.GetObject("btnReiniciar.Image")));
+            this.btnReiniciar.Location = new System.Drawing.Point(46, 393);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(68, 51);
+            this.btnReiniciar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnReiniciar.TabIndex = 13;
+            this.btnReiniciar.TabStop = false;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(1385, 766);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnPlayer);
+            this.Controls.Add(this.lbCredits);
+            this.Controls.Add(this.picture2);
+            this.Controls.Add(this.picture1);
+            this.Controls.Add(this.picturePerspective);
+            this.Controls.Add(this.pictureVideo);
+            this.Controls.Add(this.lbSimpleSubtittle);
+            this.Controls.Add(this.lbTittle);
+            this.Controls.Add(this.pictureLogo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vehículo Autónomo ACO";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureVideo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picturePerspective)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnImport)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnReiniciar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureLogo;
+        private System.Windows.Forms.Label lbTittle;
+        private System.Windows.Forms.Label lbSimpleSubtittle;
+        private System.Windows.Forms.PictureBox pictureVideo;
+        private System.Windows.Forms.PictureBox picturePerspective;
+        private System.Windows.Forms.PictureBox picture1;
+        private System.Windows.Forms.PictureBox picture2;
+        private System.Windows.Forms.Label lbCredits;
+        private System.Windows.Forms.PictureBox btnPlayer;
+        private System.Windows.Forms.PictureBox btnImport;
+        private System.Windows.Forms.PictureBox btnReiniciar;
+    }
+}
+
---
+++ FormPrincipal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vehículo_Autónomo_ACO
+{
+    public partial class FormPrincipal : Form
+    {
+        #region Variables globales
+        
+        #endregion
+
+        #region Inicialización de componentes
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+
+        #endregion
+        
+        #region Importar
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ImportarVideo()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog()==DialogResult.OK)
+            {
+
+            }                                   
+        }
+
+
+
+
+    }
+}
